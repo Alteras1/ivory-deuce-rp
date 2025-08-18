a60adaf0e0f2240864c496f7e3f873de8377cf57
Alteras1 <42795314+Alteras1@users.noreply.github.com.>
2025-08-17 21:30:09 -0700

    Change build dir

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,5 @@
 <template>
+  <NuxtRouteAnnouncer />
   <div
     class="absolute inset-0 h-full w-full bg-black bg-[radial-gradient(#f7f4ec_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_60%_60%_at_50%_50%,#000_60%,transparent_100%)] -z-10" />
   <NuxtLayout>
